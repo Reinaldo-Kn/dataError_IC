584751924c059432e39d496c67a8fae7e9a9cbe4
Reinaldo-Kn <reinaldokneto@gmail.com>
2024-01-17 17:42:37 -0300

    v0.01

---
+++ main.py
@@ -0,0 +1,10 @@
+#from DUSTAI.csv generate a graph of the Temperatura colum
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+ 
+def main():
+    print("Hello World!")
+    
