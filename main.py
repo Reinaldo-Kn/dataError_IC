d7999f36648ab3e69051ae02c754ed1e3629f341
Reinaldo-Kn <reinaldokneto@gmail.com>
2024-01-17 17:57:40 -0300

    v0.01

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-#from DUSTAI.csv generate a graph of the Temperatura colum
+#from DUSTAI.csv generate a graph of the Temperatura column
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
- 
+
+
 def main():
-    print("Hello World!")
+    #read DUSTAI.csv, use the column Temperatura and plot it, knowing that the column has a comma instead of a dot
+    df = pd.read_csv('DUSTAI.csv', sep = ';', decimal = ',')
+    df['Temperatura'].plot()
+    plt.show()
+    #print(df['Temperatura'])   
+    
+    
+
+if __name__ == '__main__':
+    main()
     
